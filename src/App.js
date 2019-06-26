d41fb53840b83d4d39e1c31ed88399ae8dd7a05b
Alyssa <allygator95@gmail.com>
2019-06-26 15:17:36 -0400

    Adding firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import config from './firebaseConfig';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const firebaseApp = firebase.initializeApp(config);
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+function App(props) {
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = props;
   return (
-    <div className="App">
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          user
+            ? <p>Hello, {user.displayName}</p>
+            : <p>Please sign in.</p>
+        }
+        {
+          user
+            ? <button onClick={signOut}>Sign out</button>
+            : <button onClick={signInWithGoogle}>Sign in with Google</button>
+        }
       </header>
     </div>
   );
 }
 
-export default App;
+
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
