4a333f0debb8e0327d35f44d415a07b091603239
Alyssa <allygator95@gmail.com>
2019-08-10 12:42:19 -0400

    switched away from a single object. still cant show checkboxes correctly

--- src/components/Add.js
+++ src/components/Add.js
@@ -4,50 +4,40 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-
 import axios from 'axios';
 
-
 function Add() {
-    const [values, setValues] = useState({
-        url: 'https://www.uniqlo.com/us/en/women-u-crew-neck-short-sleeve-t-shirt-421301.html?dwvar_421301_color=COL02#teesAnchor=&start=2&cgid=women-uniqlo-u',
-        price: null,
-        submitted: false,
-        itemData: {},
-        itemOptions: null
-    });
+    const [url, setURL] = useState('https://www.uniqlo.com/us/en/women-u-crew-neck-short-sleeve-t-shirt-421301.html?dwvar_421301_color=COL02#teesAnchor=&start=2&cgid=women-uniqlo-u');
+    const [price, setPrice] = useState(null);
+    const [submitted, setSubmitted] = useState(false);
+    const [itemData, setItem] = useState(null);
+    const [itemOptions, setOptions] = useState(null);
     const [checkboxes, setCheck] = useState({});
     const [all, setAll] = useState(false);
 
-    const [fetchReturn, setFetchData] = useState();
 
-    const handleChange = name => event => {
-        setValues({ ...values, [name]: event.target.value });
-    };
-
-    const checkHandleChange = name => event => {
-        setCheck({ ...checkboxes, [name]: event.target.checked });
-    };
 
     function getItemData(e) {
         e.preventDefault();
         let checkStatusArray = [];
-        axios.post('/.netlify/functions/getItem', {url: values.url})
+        axios.post('/.netlify/functions/getItem', {url})
             .then(function(response){
-                setFetchData(response.data);
-                // let colorsCreater = Object.keys(response.data.options).map(function(key) {
-                //     let itemColor = response.data.options[key].color;
-                //     itemColor = itemColor.replace(/\s/g, '');
-                //     checkStatusArray.push({[itemColor]: false});
-                // });
+                let colorsCreater = Object.keys(response.data.options).map(function(key) {
+                    let itemColor = response.data.options[key].color;
+                    itemColor = itemColor.replace(/\s/g, '');
+                    checkStatusArray.push({[itemColor]: false});
+                });
                 createCheckboxes(checkStatusArray);
                 let colors = Object.keys(response.data.options).map(function(key) {
                     let itemColor = response.data.options[key].color;
                     itemColor = itemColor.replace(/\s/g, '');
+                    // checkStatusArray.push({[itemColor]: false});
                     return (<FormControlLabel control={<Checkbox value={response.data.options[key].color} onChange={checkHandleChange(itemColor)} checked={checkboxes[itemColor]} />} label={response.data.options[key].color} key={response.data.options[key].color}/>);
                 })
 
-                setValues({ ...values, itemData: response.data, submitted: true, itemOptions: colors });
+                setItem(response.data);
+                setSubmitted(true);
+                setOptions(colors);
             })
             // .then(function(response){
     }
@@ -59,6 +49,7 @@ function Add() {
             let value = (Object.values(checkStatusArray[i])[0]);
             checks[key] = value;
         };
+        console.log(checks);
         setCheck(checks);
     }
 
@@ -69,9 +60,9 @@ function Add() {
         for (var i=0; i<keys.length; i++) {
             checks[keys[i]] = bool;
         }
+        console.log(checks);
         setCheck(checks);
     }
-
 
     let urlBox = (
         <form>
@@ -79,7 +70,7 @@ function Add() {
               required
               id="url"
               label="URL"
-              onChange={handleChange('url')}
+              onChange={e => setURL(e.target.value)}
               value="https://www.uniqlo.com/us/en/women-u-crew-neck-short-sleeve-t-shirt-421301.html?dwvar_421301_color=COL02#teesAnchor=&start=2&cgid=women-uniqlo-u"
             />
         <Button id="submit" variant="contained" onClick={(e)=> getItemData(e)}>
@@ -92,20 +83,30 @@ function Add() {
         <div id="colorOptionsGroup">
             <h3>Select Colors</h3>
             <div id="colorOptions">
-                {values.itemOptions}
+                {itemOptions}
             </div>
         {all ?
             (<Button variant="contained" onClick={()=> setCheckboxes(false)}>Select none</Button>)
             : (<Button variant="contained" onClick={()=> setCheckboxes(true)}>Select all </Button>)
         }
         </div>
-    )
+    );
+
+    useEffect(() => {
+        console.log(checkboxes);
+    }, [checkboxes])
+
+    const checkHandleChange = name => event => {
+        console.log(name);
+        console.log(checkboxes);
+        setCheck({ ...checkboxes, [name]: event.target.checked });
+    };
 
     return (
         <Card className="addPanel">
             <h2>Add An Item</h2>
-            {values.submitted ? "" : urlBox}
-            {values.submitted && colorOptions}
+            {submitted ? "" : urlBox}
+            {submitted && colorOptions}
         </Card>
     );
 }
